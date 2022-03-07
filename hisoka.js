37c552b2b0a10c3795f4513cbc01f390c594ed7b
IRJA <93579297+irja333@users.noreply.github.com>
2022-03-07 11:33:15 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1442,7 +1442,7 @@ break
             break
             case 'anime': case 'waifu': case 'husbu': case 'neko': case 'shinobu': case 'megumin': {
                 m.reply(mess.wait)
-                hisoka.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/'+command, 'apikey') }, caption: `Download From ${text}` }, { quoted: m})
+                hisoka.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/' + command, 'apikey') }, caption: `Download From ${text}` }, { quoted: m})
             }
             break
 	    case 'couple': {
